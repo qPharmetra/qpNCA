6d3a9cbb3491a129a1519ca2a901a3de6fe79306
krinaj <krinaj@gmail.com>
2019-03-04 15:59:22 -0800

    few more functions with documentation

--- R/nca.sumstat.conc.r
+++ R/nca.sumstat.conc.r
@@ -1,25 +1,34 @@
-#' Generate summary statistics of concentrations
+#' Summarizes a vector concentration measurements for output to a report
 #'
-#' @param x
-#' @param LOQ
-#' @param na.rm
-#' @param ns
-#' @param bloqRule
-#' @param oddCode
-#' @param bloqCode
-#' @param ...
+#' @param x character vector containing numeric values to be summarized
+#' @param LOQ limit of quantitation for x
+#' @param ns number of significant figures for summary values
+#' @param bloqRule Rule for treatment of BLOQ values "BLOQ=0" or "BLOQ=LOQ/2"
+#' @param bloqCode  alphabetic code for BLOQ values
+#' @param oddCode alphabetical codes for concentrations
 #'
-#' @return
+#' @return summary statistics of concentrations vs. time
+#' @example
+#' NTAD <- c(0,0.3,0.5,1,2,4,5,7,9,12,24)
+#' Theoph1 <- Theoph %>%
+#'  mutate(NTAD=metrumrg::snap(Time, NTAD)) %>%
+#'  mutate(Subject=as.numeric(as.character(Subject)), #converting from factor to numeric
+#'         BQL = ifelse(conc<=0.25, 1, 0),                #just adding few BLQs to demonstrate functionality
+#'         conc= ifelse(conc<=0.25, NA, conc))            #just adding few BLQs to demonstrate functionality
+#' ConcSumTab = tapply(Theoph1$conc, Theoph1$NTAD,
+#'                     nca.sumstat.conc,
+#'                     LOQ = 0.250,
+#'                     ns = 3,
+#'                     bloqRule = "BLOQ=0",
+#'                     oddCode = "M",
+#'                     bloqCode = "BLOQ") %>%
+#'              bind_cols()  #convert to data frame
+#'
+#' #add names
+#' ConcSumTab1 <- cbind(Parameter = names(ConcSumTab$`0`), ConcSumTab)
+#'
 #' @export
-nca.sumstat.conc = function(x, LOQ, na.rm = T, ns = 3, bloqRule = "BLOQ=0", oddCode = Cs(M,NS), bloqCode="BLOQ", ...){
-  # Summarize a vector concentration measurements for output to a report
-  #   x     = character vector containing numeric values to be summarized
-  #   LOQ   = limit of quantitation for x
-  #   na.rm = flag for removal of NA values
-  #   ns    = number of significant figures for summary values
-  #   bloqRule  = Rule for treatment of BLOQ values
-  #   oddCode   = alphabetical codes for concentrations
-  #   bloqCode  = alphabetic code for BLOQ values
+nca.sumstat.conc = function(x, LOQ, ns = 3, bloqRule = "BLOQ=0", oddCode = Cs(M,NS), bloqCode="BLOQ", ...){
 
   # prepopulate the output assuming nothing needs to be summarized (i.e. all(x)=="NA")
   N = 0
@@ -54,7 +63,7 @@ nca.sumstat.conc = function(x, LOQ, na.rm = T, ns = 3, bloqRule = "BLOQ=0", oddC
              c(bloqCode, bloqCode, "NC", rep(bloqCode, 3), "NC", "NC", "NC"))
   } else if(nn == 0) { # No observations or all NA
     cvec = rep("NC", 10)
-  } else if(mean(as.numeric(x), na.rm=T) < LOQ){
+  } else if(mean(as.numeric(x), na.rm=TRUE) < LOQ){
     # mean BLOQ
     x = as.numeric(x[whichNumeric(x)])
     cvec = c(N = length(x[whichNumeric(x)]),
@@ -63,7 +72,7 @@ nca.sumstat.conc = function(x, LOQ, na.rm = T, ns = 3, bloqRule = "BLOQ=0", oddC
              SD = "NC",
              Min = bloqCode,
              Median = bloqCode,
-             Max = sprintf(myfmt1, signif(max(as.numeric(x[whichNumeric(x)]), na.rm=na.rm),ns)),
+             Max = sprintf(myfmt1, signif(max(as.numeric(x[whichNumeric(x)]), na.rm=TRUE),ns)),
              CV = "NC",
              UCI = "NC",
              LCI = "NC")
@@ -74,19 +83,19 @@ nca.sumstat.conc = function(x, LOQ, na.rm = T, ns = 3, bloqRule = "BLOQ=0", oddC
     # Few observations
     N = sprintf("%.0f", nn)
     x = as.numeric(x[whichNumeric(x)])
-    Mean = sprintf(myfmt2, signif(mean(x, na.rm = na.rm), ns2))
-      if(grepl("e", Mean)) Mean = sprintf("%g", signif(mean(x, na.rm = na.rm), ns2))
-    geoMean = sprintf(myfmt2, signif(geomean(x[x>0], na.rm = na.rm), ns2))
-      if(grepl("e", geoMean)) geoMean = sprintf("%g", signif(geomean(x, na.rm = na.rm), ns2))
+    Mean = sprintf(myfmt2, signif(mean(x, na.rm=TRUE), ns2))
+      if(grepl("e", Mean)) Mean = sprintf("%g", signif(mean(x, na.rm =TRUE), ns2))
+    geoMean = sprintf(myfmt2, signif(geomean(x[x>0], na.rm =TRUE), ns2))
+      if(grepl("e", geoMean)) geoMean = sprintf("%g", signif(geomean(x, na.rm =TRUE), ns2))
     SD = "NC"
-    Min = sprintf(myfmt1, signif(min(x, na.rm=na.rm),ns))
+    Min = sprintf(myfmt1, signif(min(x, na.rm=TRUE),ns))
     if(Min < LOQ) Min = bloqCode
-      if(grepl("e", Min)) Min = sprintf("%g", signif(min(x, na.rm=T), ns)) # correct for scientific notation
-    Median = sprintf(myfmt2, signif(median(x, na.rm=na.rm), ns2))
+      if(grepl("e", Min)) Min = sprintf("%g", signif(min(x, na.rm=TRUE), ns)) # correct for scientific notation
+    Median = sprintf(myfmt2, signif(median(x, na.rm=TRUE), ns2))
       if(Median < LOQ) Median = bloqCode
-      if(grepl("e", Median)) Median = sprintf("%g", signif(mean(x, na.rm = na.rm), ns2))
-    Max = sprintf(myfmt1, signif(max(x, na.rm=na.rm), ns))
-      if(grepl("e", Max)) Max = sprintf("%g", signif(max(x, na.rm=T), ns)) # correct for scientific notation
+      if(grepl("e", Median)) Median = sprintf("%g", signif(mean(x, na.rm =TRUE), ns2))
+    Max = sprintf(myfmt1, signif(max(x, na.rm=TRUE), ns))
+      if(grepl("e", Max)) Max = sprintf("%g", signif(max(x, na.rm=TRUE), ns)) # correct for scientific notation
     CV = "NC"
     UCI = "NC"
     LCI = "NC"
@@ -103,26 +112,26 @@ nca.sumstat.conc = function(x, LOQ, na.rm = T, ns = 3, bloqRule = "BLOQ=0", oddC
     # normal case
     N = sprintf("%.0f", nn)
     x = as.numeric(x[whichNumeric(x)])
-    Mean = sprintf(myfmt2, signif(mean(x, na.rm = na.rm), ns2))
+    Mean = sprintf(myfmt2, signif(mean(x, na.rm =TRUE), ns2))
       if(grepl("e", Mean)) Mean = sprintf("%g", signif(mean(x, na.rm = na.rm), ns2))
     if(all(x[!is.na(x)]>0)) {
-      geoMean = sprintf(myfmt2, signif(geomean(x, na.rm = na.rm),ns2))
-      if(grepl("e", geoMean)) geoMean = sprintf("%g", signif(geomean(x, na.rm = na.rm), ns2))
+      geoMean = sprintf(myfmt2, signif(geomean(x, na.rm =TRUE),ns2))
+      if(grepl("e", geoMean)) geoMean = sprintf("%g", signif(geomean(x, na.rm =TRUE), ns2))
     } else geoMean = "NC"
-    SD = sprintf(myfmt2, signif(sd(x, na.rm=na.rm),ns2))
-      if(grepl("e", SD)) SD = sprintf("%g", signif(sd(x, na.rm=na.rm), ns)) # correct for scientific notation
-    Min = sprintf(myfmt1, signif(min(x, na.rm=na.rm), ns))
+    SD = sprintf(myfmt2, signif(sd(x, na.rm=TRUE),ns2))
+      if(grepl("e", SD)) SD = sprintf("%g", signif(sd(x, na.rm=TRUE), ns)) # correct for scientific notation
+    Min = sprintf(myfmt1, signif(min(x, na.rm=TRUE), ns))
       if(as.numeric(Min) < LOQ) Min = bloqCode
-      if(grepl("e", Min)) Min = sprintf("%g", signif(min(x, na.rm=na.rm), ns)) # correct for scientific notation
-    Median = sprintf(myfmt2, signif(median(x, na.rm=na.rm),ns2))
+      if(grepl("e", Min)) Min = sprintf("%g", signif(min(x, na.rm=TRUE), ns)) # correct for scientific notation
+    Median = sprintf(myfmt2, signif(median(x, na.rm=TRUE),ns2))
       if(as.numeric(Median) < LOQ) Median = bloqCode
-      if(grepl("e", Median)) Median = sprintf("%g", signif(median(x, na.rm=na.rm), ns2)) # correct for scientific notation
-    Max = sprintf(myfmt1, signif(max(x, na.rm=na.rm), ns))
-      if(grepl("e", Max)) Max = sprintf("%g", signif(max(x, na.rm=na.rm), ns)) # correct for scientific notation
-    CV = sprintf("%.1f", sd(x, na.rm=na.rm)/mean(x, na.rm = na.rm)*100)
-    se = sd(x, na.rm=na.rm)/sqrt(nn)
-    uci = mean(x, na.rm = na.rm) + qt(p=.975, df=nn-1)*se
-    lci = mean(x, na.rm = na.rm) - qt(p=.975, df=nn-1)*se
+      if(grepl("e", Median)) Median = sprintf("%g", signif(median(x, na.rm=TRUE), ns2)) # correct for scientific notation
+    Max = sprintf(myfmt1, signif(max(x, na.rm=TRUE), ns))
+      if(grepl("e", Max)) Max = sprintf("%g", signif(max(x, na.rm=TRUE), ns)) # correct for scientific notation
+    CV = sprintf("%.1f", sd(x, na.rm=TRUE)/mean(x, na.rm =TRUE)*100)
+    se = sd(x, na.rm=TRUE)/sqrt(nn)
+    uci = mean(x, na.rm =TRUE) + qt(p=.975, df=nn-1)*se
+    lci = mean(x, na.rm =TRUE) - qt(p=.975, df=nn-1)*se
     UCI = sprintf(myfmt2, signif(uci, ns2))
       if(grepl("e", UCI)) UCI = sprintf("%g", signif(uci, ns2)) # correct for scientific notation
     LCI = sprintf(myfmt2, signif(lci, ns2))
