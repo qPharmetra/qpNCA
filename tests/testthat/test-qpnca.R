0337c8e37c689782bfb650cc8965f99ddcf3da64
Tim Bergsma <bergsmat@gmail.com>
2020-05-14 10:32:41 -0400

    updates to tests

--- tests/testthat/test-qpnca.R
+++ tests/testthat/test-qpnca.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 library(qpToolkit)
+library(tidyr)
 as_csv <- function (
   x, as.is = TRUE,
   na.strings = c("", "\\s",".", "NA"),
@@ -72,15 +73,15 @@ test_results <- function(x, reg, ss, route){
 reference_results <- function(x, id, from, to, route){
   y <- x %>% filter(id == id|between(id, from, to))
   y %<>% mutate(route = route)
-  y %<>% select(id,parameter,value_excel)
-  suppressWarnings(y$value_excel %<>% as.numeric %>% round(4))
+  y %<>% select(id,parameter,value_reference)
+  suppressWarnings(y$value_reference %<>% as.numeric %>% round(4))
   y
 }
 
 merged_results <- function(x, y){
   x %<>% left_join(y)
-  x %<>% mutate(identical = as.integer(value_test == value_excel))
-  x %<>% mutate(identical = ifelse(is.na(value_excel) & is.na(value_test),1,identical))
+  x %<>% mutate(identical = as.integer(value_test == value_reference))
+  x %<>% mutate(identical = ifelse(is.na(value_reference) & is.na(value_test),1,identical))
   x %<>% mutate(identical = ifelse(is.na(identical),0,identical))
   x
 }
@@ -89,33 +90,38 @@ test_that('PO SD results are stable',{
   test <- test_results(as_csv('posd.csv'),reg="SD",ss="N",route="EV")
   refr <- reference_results(as_csv('voucher.csv'), id = -1, from = 1, to = 6, route = 'ev')
   comp <- merged_results(test,refr)
-  expect_true(all(comp$identical == 1))
+  diff <- comp %>% filter(identical == 0) %>% data.frame
+  expect_identical(comp$value_test, comp$value_reference)
 })
 
 test_that('IV SD results are stable',{
   test <- test_results(as_csv('ivsd.csv'),reg="SD",ss="N",route="IVB" )
   refr <- reference_results(as_csv('voucher.csv'), id = -2, from = 7, to = 10, route = 'ivb')
   comp <- merged_results(test,refr)
-  expect_true(all(comp$identical == 1))
+  diff <- comp %>% filter(identical == 0) %>% data.frame
+  expect_identical(comp$value_test, comp$value_reference)
 })
 
 test_that('PO MD non-steady state results are stable',{
   test <- test_results(as_csv('pomdnoss.csv'),reg="MD",ss="N",route="EV")
   refr <- reference_results(as_csv('voucher.csv'), id = -3, from = 11, to = 17, route = 'ev')
   comp <- merged_results(test,refr)
-  expect_true(all(comp$identical == 1))
+  diff <- comp %>% filter(identical == 0) %>% data.frame
+  expect_identical(comp$value_test, comp$value_reference)
 })
 
 test_that('PO MD steady state results are stable',{
   test <- test_results(as_csv('pomdss.csv'),reg="MD",ss="Y",route="EV")
   refr <- reference_results(as_csv('voucher.csv'), id = Inf, from = 18, to = 20, route = 'ev')
   comp <- merged_results(test,refr)
-  expect_true(all(comp$identical == 1))
+  diff <- comp %>% filter(identical == 0) %>% data.frame
+  expect_identical(comp$value_test, comp$value_reference)
 })
 
 test_that('IV MD results are stable',{
   test <- test_results(as_csv('ivmd.csv'),reg="MD",ss="N",route="IVB")
   refr <- reference_results(as_csv('voucher.csv'), id = -5, from = Inf, to = -Inf, route = 'ivb')
   comp <- merged_results(test,refr)
-  expect_true(all(comp$identical == 1))
+  diff <- comp %>% filter(identical == 0) %>% data.frame
+  expect_identical(comp$value_test, comp$value_reference)
 })
