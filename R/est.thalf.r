e6eb32a5e32349db61014f18537f60083bcdd0fd
Viktor Rognås 🌱 <viktor.rognas@gmail.com>
2025-06-04 22:21:22 +0200

    Update est.thalf.r

--- R/est.thalf.r
+++ R/est.thalf.r
@@ -15,6 +15,8 @@
 #' @param depvar variable name containing the dependent variable (e.g., concentration)
 #' @param includeCmax include results of regression including Cmax in selection? (y/n); x$includeCmax overrides if provided
 #' @param exclvar a variable name containing information about points to be excluded (these should have exclvar = 1)
+#' @param startth user defined start time for regression
+#' @param endth user defined end time for regression
 #' @importFrom stats lm
 #' @importFrom dplyr first last
 #' @return
@@ -30,6 +32,7 @@
 #' * **end_th time** of last sample included in the thalf estimation
 #' * **includeCmax** include results of regression including Cmax in selection? (y/n)
 #' * **points_excluded** are time points excluded from the half-life estimation? (y/n)
+#' * **usrmod** are start and/or end point of regression interval user defined? (y/n)
 #' @export
 #' @examples
 #' \donttest{
@@ -45,13 +48,18 @@
 est.thalf <- function(
   x,
   by = NULL,
-  timevar="time",
-  depvar="dv",
-  includeCmax="Y",
-  exclvar=NA){
-  if(is.null(by)) by <- as.character(groups(x))
+  timevar = "time",
+  depvar = "dv",
+  includeCmax = "Y",
+  exclvar = NA,
+  startth = NA,
+  endth = NA
+) {
+  if (is.null(by)) by <- as.character(groups(x))
   supplied <- character(0)
-  if(!missing(includeCmax)) supplied <- 'includeCmax'
+  if (!missing(includeCmax)) supplied <- c(supplied, 'includeCmax')
+  if (!missing(startth)) supplied <- c(supplied, 'startth')
+  if (!missing(endth)) supplied <- c(supplied, 'endth')
   x <- group_by_at(x, vars(by))
   x <- do(
     .data = x,
@@ -61,6 +69,8 @@ est.thalf <- function(
       depvar = depvar,
       includeCmax = includeCmax,
       exclvar = exclvar,
+      startth = startth,
+      endth = endth,
       supplied = supplied
     )
   )
@@ -73,99 +83,192 @@ est.thalf <- function(
   depvar,
   includeCmax,
   exclvar,
+  startth,
+  endth,
   supplied
-){
-
-  if('includeCmax' %in% names(x)){
-    if('includeCmax' %in% supplied){
+) {
+  if ('includeCmax' %in% names(x)) {
+    if ('includeCmax' %in% supplied) {
       warning('includeCmax supplied as column overrides like-named argument')
     }
     includeCmax <- unique(x$includeCmax)
     x$includeCmax <- NULL
   }
-  if(length(includeCmax) > 1) {
+  if (length(includeCmax) > 1) {
     warning('includeCmax has length > 1; only first value will be used')
     includeCmax <- includeCmax[[1]]
   }
-  if(!'timevar' %in% names(x))x %<>% rename(timevar = !!timevar)
-  if(!'depvar' %in% names(x))x %<>% rename(depvar = !!depvar)
 
-  if (!is.na(exclvar) & !(exclvar %in% names(x))) stop(paste("Exclusion variable",exclvar,"does not exist"), call.=F)
+  if ('startth' %in% names(x)) {
+    if (!is.na(unique(x$startth))) {
+      if ('startth' %in% supplied) {
+        warning('startth supplied as column overrides like-named argument')
+      }
+      startth <- unique(x$startth)
+      x$startth <- NULL
+    }
+    x$startth <- NULL
+  }
+  if (length(startth) > 1) {
+    warning('startth has length > 1; only first value will be used')
+    startth <- startth[[1]]
+  }
 
-  if(!(is.na(exclvar))& exclvar %in% names(x)) { x %<>% rename(exclvar = !!exclvar) }
+  if ('endth' %in% names(x)) {
+    if (!is.na(unique(x$endth))) {
+      if ('endth' %in% supplied) {
+        warning('endth supplied as column overrides like-named argument')
+      }
+      endth <- unique(x$endth)
+      x$endth <- NULL
+    }
+    x$endth <- NULL
+  }
+  if (length(endth) > 1) {
+    warning('endth has length > 1; only first value will be used')
+    endth <- endth[[1]]
+  }
+
+  if (!'timevar' %in% names(x)) x %<>% rename(timevar = !!timevar)
+  if (!'depvar' %in% names(x)) x %<>% rename(depvar = !!depvar)
+
+  if (!is.na(exclvar) & !(exclvar %in% names(x)))
+    stop(paste("Exclusion variable", exclvar, "does not exist"), call. = F)
+
+  if (!(is.na(exclvar)) & exclvar %in% names(x)) {
+    x %<>% rename(exclvar = !!exclvar)
+  }
 
   if (!is.na(exclvar)) {
-
-    anyexcl=0
-    if (any(x$exclvar==1)) {anyexcl=1}
+    usrmod = 0 # flags whether user set start and/or stop time of range
+    anyexcl = 0
+    if (any(x$exclvar == 1)) {
+      anyexcl = 1
+    }
 
     x %<>%
-      filter(exclvar!=1|is.na(exclvar))  # remove samples to be excluded from the regression
-
+      #      mutate(tmax=first(timevar[depvar==max(depvar,na.rm=T)])) %>%
+      filter(exclvar != 1 | is.na(exclvar)) # remove samples to be excluded from the regression
   }
 
   x %<>%
-    filter(!is.na(depvar)&depvar>0) %>%
-    filter(timevar>=first(timevar[depvar==max(depvar,na.rm=T)]))      # include Cmax
+    filter(!is.na(depvar) & depvar > 0) %>%
+    mutate(tmax = first(timevar[depvar == max(depvar)])) %>%
+    filter(timevar >= tmax) # greater or equal than ORIGINAL tmax (before possible exclusions)
 
-  if (tolower(includeCmax)=="n") {
-    x=x %>% filter(timevar>first(timevar[depvar==max(depvar,na.rm=T)]))   # exclude Cmax
+  if (tolower(includeCmax) == "n") {
+    x = x %>% filter(timevar > tmax) # greater than ORIGINAL tmax (before possible exclusions)
   }
 
-  est=0
-  i=length(x$timevar)-2
-  result = data.frame(matrix(ncol=7,nrow = 1))
+  # 1 both start and end: use these two, no optimalisation
 
-  if (i>=1) {
-    est=1
-    result = data.frame(matrix(ncol=7,nrow = i))
-  }
+  if (!is.na(startth) & !is.na(endth)) {
+    x %<>%
+      filter(timevar >= startth & timevar <= endth) # keep only samples between startth and endth
 
-  while (i>=1) {
-    ## make subset of data frame for each number of data points
-    xx = x[i:nrow(x), ]
-    ## execute loglinear model fit
-    lmcall=lm(log(xx$depvar)~xx$timevar)
-    lmcall.estimates = as.numeric(summary(lmcall)$coef[,"Estimate"])
+    usrmod = 1
+    est = 1
+    result = data.frame(matrix(ncol = 7, nrow = 1))
+
+    lmcall = lm(log(x$depvar) ~ x$timevar)
+    lmcall.estimates = as.numeric(summary(lmcall)$coef[, "Estimate"])
     ## save results of loglin lm fit
-    result[i,1]=length(x$timevar)-i+1
-    result[i,2]=exp(lmcall.estimates[1])  # exponentiate to see the actual intercept
-    result[i,3]=lmcall.estimates[2]*-1
-    result[i,4]=summary(lmcall)$r.squared
-    result[i,5]=summary(lmcall)$adj.r.squared
-    result[i,6]=x$timevar[i]
-    result[i,7]=last(x$timevar)
-    i=i-1
-  }
-  names(result) = c('no.points','intercept','lambda_z','r.squared','adj.r.squared','start_th','end_th')
-  if (est==1) {
-    result=result %>% mutate(sel=no.points[adj.r.squared==max(adj.r.squared)],
-                             thalf=log(2)/lambda_z,
-                             includeCmax=includeCmax
-    ) %>%
-      filter(sel==no.points) %>%
-      select(-sel)
-  }
+    result[1] = length(x$timevar)
+    result[2] = exp(lmcall.estimates[1]) # exponentiate to see the actual intercept
+    result[3] = lmcall.estimates[2] * -1
+    result[4] = summary(lmcall)$r.squared
+    result[5] = summary(lmcall)$adj.r.squared
+    result[6] = startth
+    result[7] = endth
+  } # 2 only start: remove all before that start time, then start optimalisation
   else {
-    result=result %>% mutate(no.points=NA,intercept=NA,lambda_z=NA,r.squared=NA,adj.r.squared=NA,thalf=NA,
-                             start_th=NA,end_th=NA)
+    if (!is.na(startth) & is.na(endth)) {
+      usrmod = 1
+      x %<>%
+        filter(timevar >= startth) # keep only samples from startth to the end
+    }
+
+    # 3 only end: remove all after that end time, then start optimalisation
+
+    if (is.na(startth) & !is.na(endth)) {
+      usrmod = 1
+      x %<>%
+        filter(timevar <= endth) # keep only samples from start to endth
+    }
+
+    est = 0
+    i = length(x$timevar) - 2
+    result = data.frame(matrix(ncol = 7, nrow = 1))
+
+    if (i >= 1) {
+      est = 1
+      result = data.frame(matrix(ncol = 7, nrow = i))
+    }
+
+    while (i >= 1) {
+      ## make subset of data frame for each number of data points
+      xx = x[i:nrow(x), ]
+      ## execute loglinear model fit
+      lmcall = lm(log(xx$depvar) ~ xx$timevar)
+      lmcall.estimates = as.numeric(summary(lmcall)$coef[, "Estimate"])
+      ## save results of loglin lm fit
+      result[i, 1] = length(x$timevar) - i + 1
+      result[i, 2] = exp(lmcall.estimates[1]) # exponentiate to see the actual intercept
+      result[i, 3] = lmcall.estimates[2] * -1
+      result[i, 4] = summary(lmcall)$r.squared
+      result[i, 5] = summary(lmcall)$adj.r.squared
+      result[i, 6] = x$timevar[i]
+      result[i, 7] = last(x$timevar)
+      i = i - 1
+    }
+  }
+  names(result) = c(
+    'no.points',
+    'intercept',
+    'lambda_z',
+    'r.squared',
+    'adj.r.squared',
+    'start_th',
+    'end_th'
+  )
+  if (est == 1) {
+    result = result %>%
+      mutate(
+        sel = no.points[adj.r.squared == max(adj.r.squared)],
+        thalf = log(2) / lambda_z,
+        includeCmax = includeCmax
+      ) %>%
+      filter(sel == no.points) %>%
+      select(-sel)
+  } else {
+    result = result %>%
+      mutate(
+        no.points = NA,
+        intercept = NA,
+        lambda_z = NA,
+        r.squared = NA,
+        adj.r.squared = NA,
+        thalf = NA,
+        start_th = NA,
+        end_th = NA
+      )
   }
 
   if (!is.na(exclvar)) {
-
-    if (anyexcl==1) {
+    if (anyexcl == 1) {
       result = result %>%
-        mutate(points_excluded="Y")
-    }
-    else {
+        mutate(points_excluded = "Y")
+    } else {
       result = result %>%
-        mutate(points_excluded="N")
+        mutate(points_excluded = "N")
     }
-  }
-  else {
+  } else {
     result = result %>%
-      mutate(points_excluded="N")
+      mutate(points_excluded = "N")
   }
+
+  result = result %>%
+    mutate(usrmod = ifelse(usrmod == 1, "Y", "N"))
 
   return(result)
 }
