89fb11a6e096fb54a3f2a5a408ab641485e05c11
Viktor Rognås 🌱 <viktor.rognas@gmail.com>
2025-06-05 12:30:34 +0200

    Fix log trapezoid functions to avoid invalid log

--- R/trap.r
+++ R/trap.r
@@ -20,30 +20,33 @@ trap <- function(x = NA, y = NA, method = 1){
   if (method == 1){
     z <- sum( (x-lag(x)) * (y+lag(y)) / 2, na.rm = T )
   }
-  if (method==2) {
-    z <- sum(
-      ifelse(
-        lag(y) > y & lag(y) > 0 & y > 0,
-        (x - lag(x)) * (y - lag(y)) / log(y/lag(y)), # See Rowland & Tozer 471
-        (x - lag(x)) * (y + lag(y)) / 2
-      ),
-      na.rm=T
-    )
+  if (method == 2) {
+    dx <- diff(x)
+    y2 <- y[-1]
+    y1 <- y[-length(y)]
+    down <- which(y1 > y2 & y1 > 0 & y2 > 0)
+    up   <- setdiff(seq_along(dx), down)
+    z <- 0
+    if (length(down)) {
+      z <- z + sum(dx[down] * (y2[down] - y1[down]) / log(y2[down]/y1[down]), na.rm = TRUE)
+    }
+    if (length(up)) {
+      z <- z + sum(dx[up] * (y2[up] + y1[up]) / 2, na.rm = TRUE)
+    }
   }
-  if (method==3) {
-    z <- sum(
-      ifelse(
-
-        x > tmax    &
-        lag(y) > 0  &
-        y > 0       &
-        lag(y) != y,
-
-        (x-lag(x))*(y-lag(y))/log(y/lag(y)),
-        (x-lag(x))*(y+lag(y))/2
-      ),
-      na.rm=T
-    )
+  if (method == 3) {
+    dx <- diff(x)
+    y2 <- y[-1]
+    y1 <- y[-length(y)]
+    logidx <- which(x[-1] > tmax & y1 > 0 & y2 > 0 & y1 != y2)
+    linidx <- setdiff(seq_along(dx), logidx)
+    z <- 0
+    if (length(logidx)) {
+      z <- z + sum(dx[logidx] * (y2[logidx] - y1[logidx]) / log(y2[logidx]/y1[logidx]), na.rm = TRUE)
+    }
+    if (length(linidx)) {
+      z <- z + sum(dx[linidx] * (y2[linidx] + y1[linidx]) / 2, na.rm = TRUE)
+    }
   }
 
   return(z)
